ec7f0836fd6df63c17a61d73104a8b5963d868b4
doduoduoniaodo <123maqianyi@gmail.com>
2024-10-05 19:18:18 -0400

    CCC '22 S2 - Good Groups

--- 2022/s2.py
+++ 2022/s2.py
@@ -18,3 +18,27 @@ for _ in range(int(input())):
                 ans += 1
 
 print(ans)
+
+
+'''
+import sys
+input = sys.stdin.readline
+
+x = [(input().strip().split()) for _ in range(int(input()))]
+y = [(input().strip().split()) for _ in range(int(input()))]
+d = {}
+
+for i in range(int(input())):
+    a, b, c = input().strip().split()
+    d[a] = d[b] = d[c] = i
+
+ans = 0
+for a, b in x:
+    ans += d[a] != d[b]
+for a, b in y:
+    ans += d[a] == d[b]
+#True --> 1
+#False --> 0
+
+print(ans)
+'''
