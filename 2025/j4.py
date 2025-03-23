a57a8616db494ffc230f8750d6c52dfb5a753cd1
QM <147770740+doduoduoniaodo@users.noreply.github.com>
2025-03-22 20:44:18 -0400

    CCC '25 J4 - Sunny Days

--- 2025/j4.py
+++ 2025/j4.py
@@ -1,5 +1,12 @@
+# Solution 1:
+import sys
+
 n = int(input())
 weathers = [input() for _ in range(n)]
+
+if 'P' not in weathers:
+    print(n-1)
+    sys.exit()
 
 left = 0
 rain = 0
@@ -16,7 +23,33 @@ for right in range(n):
     
     ans = max(ans, right - left + 1)
 
-if 'P' not in weathers:
-    ans -= 1
-
 print(ans)
+
+
+# Solution 2: 
+n = int(input())
+a = []
+start = -1
+mx = 0
+for i in range(n):
+    c = input()
+    if c == 'P':
+        if start != -1:
+            a.append((start, i-1))
+            mx = max(mx, i-start)
+            start = -1
+    else:
+        if start == -1:
+            start = i
+if start != -1:
+    a.append((start, n-1))
+    mx = max(mx, n-start)
+if mx == n:
+    print(n-1)
+else:
+    ans = mx+1
+    for i in range(1, len(a)):
+        if a[i-1][1] + 2 == a[i][0]:
+            ans = max(ans, a[i][1] - a[i-1][0] + 1)
+    print(ans)
+
