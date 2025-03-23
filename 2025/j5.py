6b7c140d9a41fa6879f74c49d12fbeacf00728de
QM <147770740+doduoduoniaodo@users.noreply.github.com>
2025-03-22 20:46:05 -0400

    CCC '25 J5 - Connecting Territories

--- 2025/j5.py
+++ 2025/j5.py
@@ -1,3 +1,4 @@
+# Solution 1:
 import sys
 input = sys.stdin.readline
 
@@ -23,3 +24,20 @@ for i in range(1, r):
     dp_prev, dp_curr = dp_curr, dp_prev
 
 print(min(dp_prev))
+
+
+# Solution 2:
+import sys
+input = sys.stdin.readline
+
+def get_cell(i, j):
+    return (i*c + j - 1) % m
+
+r, c, m = int(input()), int(input()), int(input())
+dp_prev = [float('inf')] + [i%m for i in range(c)] + [float('inf')]
+dp_curr = [float('inf')] + [0]*c + [float('inf')]
+for i in range(1, r):
+    for j in range(1, c+1):
+        dp_curr[j] = min(dp_prev[j-1], dp_prev[j], dp_prev[j+1]) + get_cell(i, j)
+    dp_prev, dp_curr = dp_curr, dp_prev
+print(min(dp_prev) + r)
