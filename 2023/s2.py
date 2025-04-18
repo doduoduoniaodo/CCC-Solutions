3129f19db482c72e444a8c93b927dd5dc4de2626
QM <147770740+doduoduoniaodo@users.noreply.github.com>
2025-04-17 20:11:08 -0400

    CCC '23 S2 - Symmetric Mountains

--- 2023/s2.py
+++ 2023/s2.py
@@ -16,3 +16,24 @@ for length in range(1, n+1):
         ans[length] = min(ans[length], memo[l][r])
 
 print(*ans[1:])
+
+
+'''
+import sys
+input = sys.stdin.readline
+n = int(input())
+a = list(map(int, input().split()))
+ans = [float('inf')]*(n+1)
+for i in range(n):
+    val, l, r = 0, i, i
+    while 0<=l and r < n:
+        val += abs(a[l] - a[r])
+        ans[r-l+1] = min(ans[r-l+1], val)
+        l, r = l-1, r+1
+    val, l, r = 0, i, i+1
+    while 0<=l and r < n:
+        val += abs(a[l] - a[r])
+        ans[r-l+1] = min(ans[r-l+1], val)
+        l, r = l-1, r+1
+print(*ans[1:n+1])
+'''
